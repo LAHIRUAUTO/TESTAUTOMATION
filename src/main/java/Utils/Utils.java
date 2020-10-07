e46ba5ffcad93c788b5f3f0251bfc5727ce78428
LAHIRU <lahiru.vikasitha@avtra.com>
2020-10-07 18:16:24 +0530

    Cross browser testing

--- src/main/java/Utils/Utils.java
+++ src/main/java/Utils/Utils.java
@@ -3,10 +3,12 @@ package Utils;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -35,10 +37,12 @@ public class Utils {
         Properties obj = new Properties();
         obj.load(objfile);
         String setChromeDriver = obj.getProperty("ChromeDriver");
+        String setFirefoxDriver = obj.getProperty("FirefoxDriver");
         String setGeckoDriver = obj.getProperty("GeckoDriver");
 
 
         System.setProperty(setGeckoDriver, setChromeDriver);
+        System.setProperty(setGeckoDriver, setFirefoxDriver);
 
         //Enable headless browser testing
         /*// Create Object of ChromeOption Class
@@ -49,13 +53,20 @@ public class Utils {
 
         //driver = new ChromeDriver(option);
 
-        driver = new ChromeDriver();
+        /*driver = new ChromeDriver();
         driver.get("https://dcsqa.avtra.com/dcs/#/login/en/IR");
-        driver.manage().window().maximize();
+        driver.manage().window().maximize();*/
 
-/*        driver2 = new ChromeDriver();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities = DesiredCapabilities.firefox();
+        capabilities.setBrowserName("firefox");
+        //capabilities.setVersion("your firefox version");
+        capabilities.setPlatform(Platform.LINUX);
+        capabilities.setCapability("marionette", false);
+        driver2 = new FirefoxDriver(capabilities);
         driver2.get("https://dcsqa.avtra.com/dcs/#/login/en/IR");
-        driver2.manage().window().maximize();*/
+        driver2.manage().window().maximize();
 
 
     }
